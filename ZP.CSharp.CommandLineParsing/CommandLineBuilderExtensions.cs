a6e95e8bf0806bc229af73f7529848d7f8bd79f8
Zhu Panda <zhupandahugo@gmail.com>
2022-02-14 15:06:47 +0800

    add proj contents

---
+++ ZP.CSharp.CommandLineParsing/CommandLineBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+namespace ZP.CSharp.CommandLineParsing
+{
+    public static class CommandLineBuilderExtensions
+    {
+        public static CommandLineBuilder UseDefaultsWithCustomHelpAndVersionOpts(
+            this CommandLineBuilder builder,
+            string[] helpAliases,
+            string[] versionAliases)
+        {
+            return builder
+                .UseVersionOption(versionAliases)
+                .UseHelp(helpAliases)
+                .UseEnvironmentVariableDirective()
+                .UseParseDirective()
+                .UseSuggestDirective()
+                .RegisterWithDotnetSuggest()
+                .UseTypoCorrections()
+                .UseParseErrorReporting()
+                .UseExceptionHandler()
+                .CancelOnProcessTermination();
+        }
+        public static CommandLineBuilder UseDefaultsWithCustomHelpOpts(
+            this CommandLineBuilder builder,
+            string[] helpAliases)
+        {
+            return builder
+                .UseVersionOption()
+                .UseHelp(helpAliases)
+                .UseEnvironmentVariableDirective()
+                .UseParseDirective()
+                .UseSuggestDirective()
+                .RegisterWithDotnetSuggest()
+                .UseTypoCorrections()
+                .UseParseErrorReporting()
+                .UseExceptionHandler()
+                .CancelOnProcessTermination();
+        }
+        public static CommandLineBuilder UseDefaultsWithCustomVersionOpts(
+            this CommandLineBuilder builder,
+            string[] versionAliases)
+        {
+            return builder
+                .UseVersionOption(versionAliases)
+                .UseHelp()
+                .UseEnvironmentVariableDirective()
+                .UseParseDirective()
+                .UseSuggestDirective()
+                .RegisterWithDotnetSuggest()
+                .UseTypoCorrections()
+                .UseParseErrorReporting()
+                .UseExceptionHandler()
+                .CancelOnProcessTermination();
+        }
+    }
+}
